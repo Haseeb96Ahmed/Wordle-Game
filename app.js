fad76f0c3f45345d439b8cf763599e119f9a05e0
Haseeb96Ahmed <ha3137648@gmail.com>
2022-02-20 13:02:21 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const tileDisplay = document.querySelector('.tile-container')
+const keyboard = document.querySelector('.key-container')
+
+const keys = [
+    'Q',
+    'W',
+    'E',
+    'R',
+    'T',
+    'Y',
+    'U',
+    'I',
+    'O',
+    'P',
+    'A',
+    'S',
+    'D',
+    'F',
+    'G',
+    'H',
+    'J',
+    'K',
+    'L',
+    'Z',
+    'X',
+    'C',
+    'V',
+    'B',
+    'N',
+    'M',
+    'ENTER',
+    '«',
+]
+
+const handleClick = () => {
+    console.assert.log('clicked')
+}
+keys.forEach(key => {
+    const buttonElement = document.createElement('button')
+    buttonElement.textContent = key
+    buttonElement.setAttribute('id', key)
+    buttonElement.addEventListener('click', handleClick)
+    keyboard.append(buttonElement)
+})
